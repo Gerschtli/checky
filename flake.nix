ae619abd3a44bee8bdb76348640c097948c8df6a
Tobias Happ <tobias.happ@gmx.de>
2025-09-24 20:29:33 +0200

    chore: add nix setup

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs [ "aarch64-darwin" "x86_64-linux" ];
+
+      pkgsFor = forEachSystem (system: import nixpkgs { inherit system; });
+
+      nodejsFor = forEachSystem (system: pkgsFor.${system}.nodejs_24);
+    in
+    {
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = pkgsFor.${system};
+            nodejs = nodejsFor.${system};
+          in
+          {
+            default = pkgs.mkShell
+              {
+                packages = [
+                  nodejs
+                  nodejs.pkgs.pnpm
+                  pkgs.sqlite-interactive
+                ];
+              };
+          });
+    };
+}
