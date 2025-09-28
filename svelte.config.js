29ab2785daf5b2ad1d34178378ce52d0c61fafb4
Tobias Happ <tobias.happ@gmx.de>
2025-09-28 21:37:50 +0200

    chore: setup vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,7 +7,9 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			regions: 'eu-west-1',
+		}),
 		experimental: {
 			remoteFunctions: true,
 		},
