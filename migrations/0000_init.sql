7412303a5f6cf76703be5678f7b05ac5c1f996c0
Tobias Happ <tobias.happ@gmx.de>
2025-09-28 22:20:53 +0200

    fix: fix migrations

--- migrations/0000_init.sql
+++ migrations/0000_init.sql
@@ -2,6 +2,8 @@ CREATE TABLE `sessions` (
 	`id` text PRIMARY KEY NOT NULL,
 	`user_id` text NOT NULL,
 	`expires_at` integer NOT NULL,
+	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
+	`updated_at` integer NOT NULL,
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
@@ -13,9 +15,9 @@ CREATE TABLE `tasks` (
 	`interval_days` integer NOT NULL,
 	`repeat_mode` text NOT NULL,
 	`archived` integer NOT NULL,
+	`archived_at` integer,
 	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
 	`updated_at` integer NOT NULL,
-	`archived_at` integer,
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
@@ -25,6 +27,8 @@ CREATE TABLE `tasks_completed` (
 	`task_id` integer NOT NULL,
 	`due_date` text NOT NULL,
 	`completion_date` text NOT NULL,
+	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
+	`updated_at` integer NOT NULL,
 	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade,
 	FOREIGN KEY (`task_id`) REFERENCES `tasks`(`id`) ON UPDATE no action ON DELETE cascade
 );
@@ -33,7 +37,9 @@ CREATE UNIQUE INDEX `tasks_completed_idx_task_id_completion_date` ON `tasks_comp
 CREATE TABLE `users` (
 	`id` text PRIMARY KEY NOT NULL,
 	`username` text NOT NULL,
-	`password_hash` text NOT NULL
+	`password_hash` text NOT NULL,
+	`created_at` integer DEFAULT (unixepoch()) NOT NULL,
+	`updated_at` integer NOT NULL
 );
 --> statement-breakpoint
 CREATE UNIQUE INDEX `users_username_unique` ON `users` (`username`);
