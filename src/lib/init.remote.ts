c516ea72090094f4542bfda324a056f5e9af6379
Tobias Happ <tobias.happ@gmx.de>
2025-09-30 21:10:49 +0200

    chore: generate test data in relation to now

--- src/lib/init.remote.ts
+++ src/lib/init.remote.ts
@@ -10,84 +10,86 @@ import { getAllTasks } from './task.remote';
 export const initData = command(async () => {
 	const user = await getUser();
 
+	const now = LocalDate.now();
+
 	const tasks = [
 		{
 			title: 'Informationen für Tag notieren',
-			nextDueDate: LocalDate.fromIsoString('2025-09-27'),
+			nextDueDate: now,
 			intervalCount: 1,
 			intervalType: 'days' as const,
 			repeatMode: 'fromDueDate' as const,
 			completions: [
 				{
-					dueDate: LocalDate.fromIsoString('2025-09-22'),
-					completionDate: LocalDate.fromIsoString('2025-09-22'),
+					dueDate: now.addDays(-5),
+					completionDate: now.addDays(-5),
 				},
 				{
-					dueDate: LocalDate.fromIsoString('2025-09-23'),
-					completionDate: LocalDate.fromIsoString('2025-09-23'),
+					dueDate: now.addDays(-4),
+					completionDate: now.addDays(-4),
 				},
 				{
-					dueDate: LocalDate.fromIsoString('2025-09-24'),
-					completionDate: LocalDate.fromIsoString('2025-09-24'),
+					dueDate: now.addDays(-3),
+					completionDate: now.addDays(-3),
 				},
 				{
-					dueDate: LocalDate.fromIsoString('2025-09-25'),
-					completionDate: LocalDate.fromIsoString('2025-09-25'),
+					dueDate: now.addDays(-2),
+					completionDate: now.addDays(-2),
 				},
 				{
-					dueDate: LocalDate.fromIsoString('2025-09-26'),
-					completionDate: LocalDate.fromIsoString('2025-09-26'),
+					dueDate: now.addDays(-1),
+					completionDate: now.addDays(-1),
 				},
 			],
 		},
 		{
 			title: 'Putzen',
-			nextDueDate: LocalDate.fromIsoString('2025-10-02'),
+			nextDueDate: now.addDays(5),
 			intervalCount: 7,
 			intervalType: 'days' as const,
 			repeatMode: 'fromCompletionDate' as const,
 			completions: [
 				{
-					dueDate: LocalDate.fromIsoString('2025-09-24'),
-					completionDate: LocalDate.fromIsoString('2025-09-25'),
+					dueDate: now.addDays(-3),
+					completionDate: now.addDays(-2),
 				},
 				{
-					dueDate: LocalDate.fromIsoString('2025-09-17'),
-					completionDate: LocalDate.fromIsoString('2025-09-17'),
+					dueDate: now.addDays(-10),
+					completionDate: now.addDays(-10),
 				},
 			],
 		},
 		{
 			title: 'Uhr aufladen',
-			nextDueDate: LocalDate.fromIsoString('2025-10-01'),
+			nextDueDate: now.addDays(4),
 			intervalCount: 12,
 			intervalType: 'days' as const,
 			repeatMode: 'fromCompletionDate' as const,
 			completions: [
 				{
-					dueDate: LocalDate.fromIsoString('2025-10-01').addDays(-12),
-					completionDate: LocalDate.fromIsoString('2025-10-01').addDays(-12),
+					dueDate: now.addDays(4).addDays(-12),
+					completionDate: now.addDays(4).addDays(-12),
 				},
 				{
-					dueDate: LocalDate.fromIsoString('2025-10-01').addDays(-28),
-					completionDate: LocalDate.fromIsoString('2025-10-01').addDays(-24),
+					dueDate: now.addDays(4).addDays(-28),
+					completionDate: now.addDays(4).addDays(-24),
 				},
 			],
 		},
 		{
 			title: 'Monatsabschluss',
-			nextDueDate: LocalDate.fromIsoString('2025-10-15'),
+			nextDueDate: now.addDays(8),
 			intervalCount: 1,
 			intervalType: 'months' as const,
 			repeatMode: 'fromCompletionDate' as const,
 			completions: [
 				{
-					dueDate: LocalDate.fromIsoString('2025-10-15').addMonths(-1),
-					completionDate: LocalDate.fromIsoString('2025-10-15').addMonths(-1),
+					dueDate: now.addDays(8).addMonths(-1),
+					completionDate: now.addDays(8).addMonths(-1),
 				},
 				{
-					dueDate: LocalDate.fromIsoString('2025-10-15').addMonths(-2),
-					completionDate: LocalDate.fromIsoString('2025-10-15').addMonths(-2),
+					dueDate: now.addDays(8).addMonths(-2),
+					completionDate: now.addDays(8).addMonths(-2),
 				},
 			],
 		},
